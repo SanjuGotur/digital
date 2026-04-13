89507b20aed4f3f03fac4fb056f5e35217115402
sanju585 <sanjugotur8@gmail.com>
2026-04-13 20:41:39 +0530

    deploye

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
-    // base: '/brand/',npm install'
+    base: '/brand/',
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
